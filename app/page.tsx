33f14212180d12b77fe8419eaf436405594622b6
square3ang <square3ang@gmail.com>
2025-03-15 15:40:32 +0900

    asdfffdsa

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 import TopBar from "../components/topbar";
 
-import { ChatBubbleLeftRightIcon, MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import { ChatBubbleLeftRightIcon, MagnifyingGlassIcon, BookOpenIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
     const btnstyl = "ml-3 mr-3 p-5 bg-gray-600 text-white drop-shadow-2xl rounded-full hover:bg-gray-800 transition duration-250 hover:scale-110";
@@ -18,7 +18,8 @@ export default function Home() {
                 <span className="text-xl lg:text-5xl drop-shadow-2xl">정사각형의 사이트에 오신 것을 환영합니다 :D</span>
                 <div className="flex mt-6 text-xl lg:text-2xl">
                     <Link className={btnstyl} href="https://squarediscord.uzu.kr/"><ChatBubbleLeftRightIcon width={28} className="m-auto lg:inline lg:h-8" /> 얼불춤 모드 Discord</Link>
-                    <Link className={btnstyl} href="https://wiki.square3ang.kr/"><MagnifyingGlassIcon width={28} className="m-auto lg:inline lg:h-8" /> 정사각위키</Link>
+                    <Link className={btnstyl} href="https://wiki.square3ang.kr/"><BookOpenIcon width={28} className="m-auto lg:inline lg:h-8" /> 정사각위키</Link>
+                    <Link className={btnstyl} href="https://wiki.square3ang.kr/"><MagnifyingGlassIcon width={28} className="m-auto lg:inline lg:h-8" /> 얼불춤 SW 헬퍼</Link>
                 </div>
                 <div className="h-32" />
             </div>
