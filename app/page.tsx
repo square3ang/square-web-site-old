5413b008c1d783c4bdf4b0318e9bac0b16364646
square3ang <square3ang@gmail.com>
2025-04-06 08:33:40 +0900

    mobile fix & logo change

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         <div className="w-full h-full">
             <div className="h-32" />
             <div className="asdf flex flex-col mt-32 w-full place-items-center justify-center text-white">
-                <img src="/squaretransparent.png" alt="Square Logo" width={250} height={250} className="animate-bounce w-40 lg:w-72" />
+                <img src="/squaretransparent.png" alt="Square Logo" width={250} height={250} className="w-40 lg:w-72" />
                 <span className="text-xl lg:text-5xl drop-shadow-2xl">정사각형의 사이트에 오신 것을 환영합니다 :D</span>
-                <div className="flex mt-6 text-xl lg:text-2xl">
+                <div className="flex flex-col lg:flex-row mt-6 text-xl text-center lg:text-2xl space-y-3 lg:space-y-0">
                     <Link className={btnstyl} href="https://squarediscord.uzu.kr/"><ChatBubbleLeftRightIcon width={28} className="m-auto lg:inline lg:h-8" /> 얼불춤 모드 Discord</Link>
                     <Link className={btnstyl} href="https://wiki.square3ang.kr/"><BookOpenIcon width={28} className="m-auto lg:inline lg:h-8" /> 정사각위키</Link>
                     <Link className={btnstyl} href="https://ash.square3ang.kr/"><MagnifyingGlassIcon width={28} className="m-auto lg:inline lg:h-8" /> 얼불춤 SW 헬퍼</Link>
                 </div>
-                <div className="mt-10 flex space-x-5">
+                <div className="mt-10 flex flex-col lg:flex-row lg:space-x-5 space-y-5 lg:space-y-0">
                     <a href="https://www.buymeacoffee.com/square3ang" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{ height: "60px !important", width: "217px !important" }} /></a>
                     <a href="https://toon.at/donate/square3ang"><img src="./kr_blue_a-1.png" style={{ height: "60px !important", width: "192px !important" }} /></a>
                 </div>
